5eccc5ba2058734af3e335a151d2a56e70fc7a48
romanbelaire <romanbelaire@gmail.com>
2019-06-08 18:30:06 -0700

    Added csv writer.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GreenThumb
+
+sensor.py logs information about the environment and saves it in a csv log. Data entries in order are: Temperature (F), relative humidity, and light (binary value).
--- sensor.py
+++ sensor.py
@@ -23,34 +23,37 @@ SI7021 SCL = pin 5
 """
 gpio.setup(LDR, gpio.IN)
 
+def logEnv(writer):
+    light = gpio.input(LDR)
+    bus.write_byte(0x40,0xF5)
+
+    time.sleep(0.3)
+
+    #SI7021 Address, 0x40 read 2 bytes humidity
+    data0 = bus.read_byte(0x40)
+    data1 = bus.read_byte(0x40)
+
+    #convert Data
+    humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
+    time.sleep(0.3)
+    bus.write_byte(0x40, 0xF3)
+    time.sleep(0.3)
+
+    #SI7021 Address, 0x40 read 2 bytes temp
+    data0 = bus.read_byte(0x40)
+    data1 = bus.read_byte(0x40)
+
+    #convert data and output it
+    cTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
+    fTemp = cTemp * 1.8 + 32
+
+    print("Relative humidity : " + str(humidity))
+    print("Temp: " + str(fTemp))
+    print("Light: " + str(light))
+    writer.writerow([fTemp, humidity, light])
+
 bus = smbus.SMBus(1)
-with open('log' + str(time.time()%10) + '.csv', 'w') as csvfile:
+with open('log.csv', 'w') as csvfile:
     writer = csv.writer(csvfile, quotechar='|', quoting=csv.QUOTE_MINIMAL)
     while True:
-        light = gpio.input(LDR)
-        bus.write_byte(0x40,0xF5)
-
-        time.sleep(0.3)
-
-        #SI7021 Address, 0x40 read 2 bytes humidity
-        data0 = bus.read_byte(0x40)
-        data1 = bus.read_byte(0x40)
-
-        #convert Data
-        humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
-        time.sleep(0.3)
-        bus.write_byte(0x40, 0xF3)
-        time.sleep(0.3)
-
-        #SI7021 Address, 0x40 read 2 bytes temp
-        data0 = bus.read_byte(0x40)
-        data1 = bus.read_byte(0x40)
-
-        #convert data and output it
-        cTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
-        fTemp = cTemp * 1.8 + 32
-
-        print("Relative humidity : " + str(humidity))
-        print("Temp: " + str(fTemp))
-        print("Light: " + str(light))
-        writer.writerow([fTemp, humidity, light])
+        logEnv(writer)
